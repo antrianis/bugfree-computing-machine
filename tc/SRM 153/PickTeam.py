0d9690351cb860ac17c860b744f0e4ef68f9e2cc
Antriani Stylianou <antriani.s@gmail.com>
2014-05-24 20:26:16 +0200

    new srms

---
+++ tc/SRM 153/PickTeam.py
@@ -0,0 +1,123 @@
+# -*- coding: utf-8 -*-
+import math,string,itertools,fractions,heapq,collections,re,array,bisect
+import sys
+
+class PickTeam:
+    def pickPeople(self, teamSize, people):
+        subsets = list(itertools.combinations(range(0,len(people)), teamSize))
+        maxs = -sys.maxint-1
+        maxPeople = ()
+        for s in subsets:
+            tsum = 0
+            for i in s:
+                j = i + 1
+                for j in s:
+                    tsum+=int(people[i].split()[j + 1])
+            if tsum > maxs:
+
+                maxs = tsum
+                maxPeople = s
+
+        names = []
+        for i in maxPeople:
+            names.append(people[i].split()[0])
+
+        names.sort()
+        return names
+
+
+# CUT begin
+# TEST CODE FOR PYTHON {{{
+import sys, time, math
+
+def tc_equal(expected, received):
+    try:
+        _t = type(expected)
+        received = _t(received)
+        if _t == list or _t == tuple:
+            if len(expected) != len(received): return False
+            return all(tc_equal(e, r) for (e, r) in zip(expected, received))
+        elif _t == float:
+            eps = 1e-9
+            d = abs(received - expected)
+            return not math.isnan(received) and not math.isnan(expected) and d <= eps * max(1.0, abs(expected))
+        else:
+            return expected == received
+    except:
+        return False
+
+def pretty_str(x):
+    if type(x) == str:
+        return '"%s"' % x
+    elif type(x) == tuple:
+        return '(%s)' % (','.join( (pretty_str(y) for y in x) ) )
+    else:
+        return str(x)
+
+def do_test(teamSize, people, __expected):
+    startTime = time.time()
+    instance = PickTeam()
+    exception = None
+    try:
+        __result = instance.pickPeople(teamSize, people);
+    except:
+        import traceback
+        exception = traceback.format_exc()
+    elapsed = time.time() - startTime   # in sec
+
+    if exception is not None:
+        sys.stdout.write("RUNTIME ERROR: \n")
+        sys.stdout.write(exception + "\n")
+        return 0
+
+    if tc_equal(__expected, __result):
+        sys.stdout.write("PASSED! " + ("(%.3f seconds)" % elapsed) + "\n")
+        return 1
+    else:
+        sys.stdout.write("FAILED! " + ("(%.3f seconds)" % elapsed) + "\n")
+        sys.stdout.write("           Expected: " + pretty_str(__expected) + "\n")
+        sys.stdout.write("           Received: " + pretty_str(__result) + "\n")
+        return 0
+
+def run_tests():
+    sys.stdout.write("PickTeam (1000 Points)\n\n")
+
+    passed = cases = 0
+    case_set = set()
+    for arg in sys.argv[1:]:
+        case_set.add(int(arg))
+
+    with open("PickTeam.sample", "r") as f:
+        while True:
+            label = f.readline()
+            if not label.startswith("--"): break
+
+            teamSize = int(f.readline().rstrip())
+            people = []
+            for i in range(0, int(f.readline())):
+                people.append(f.readline().rstrip())
+            people = tuple(people)
+            f.readline()
+            __answer = []
+            for i in range(0, int(f.readline())):
+                __answer.append(f.readline().rstrip())
+            __answer = tuple(__answer)
+
+            cases += 1
+            if len(case_set) > 0 and (cases - 1) in case_set: continue
+            sys.stdout.write("  Testcase #%d ... " % (cases - 1))
+            passed += do_test(teamSize, people, __answer)
+
+    sys.stdout.write("\nPassed : %d / %d cases\n" % (passed, cases))
+
+    T = time.time() - 1400699258
+    PT, TT = (T / 60.0, 75.0)
+    points = 1000 * (0.3 + (0.7 * TT * TT) / (10.0 * PT * PT + TT * TT))
+    sys.stdout.write("Time   : %d minutes %d secs\n" % (int(T/60), T%60))
+    sys.stdout.write("Score  : %.2f points\n" % points)
+
+if __name__ == '__main__':
+    run_tests()
+
+# }}}
+# CUT end
