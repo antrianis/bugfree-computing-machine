b39437fc597058984de12eb73317f1dd3ce67f4d
Antriani Stylianou <antriani.s@gmail.com>
2015-01-24 19:37:33 +0100

    remoe

--- utilities/generate.py
+++
@@ -1,9 +0,0 @@
-import os
-import random
-f = open("sample.txt","w")
-lineno=random.randint(0,1000000)
-divisor=random.randint(0,10000000)
-f.write(str(lineno) + " " + str(divisor) + "\n")
-for i in range(0,lineno):
-    f.write(str(random.randint(0,1000000000)) + "\n")
-f.close()
